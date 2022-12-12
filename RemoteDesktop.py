320d7c1eb82c88f53dde42b38600b9318cdb8ea2
Esteb37 <esteban37padilla@gmail.com>
2022-12-11 23:57:09 -0600

    Hardware turn on

--- RemoteDesktop.py
+++ RemoteDesktop.py
@@ -3,6 +3,18 @@ import time
 import serial
 import psutil
 import subprocess
+import win32api
+import win32con
+
+
+def turn_off_monitor():
+    win32api.SendMessage(win32con.HWND_BROADCAST,
+                         win32con.WM_SYSCOMMAND, win32con.SC_MONITORPOWER, 2)
+
+
+def turn_on_monitor():
+    win32api.SendMessage(win32con.HWND_BROADCAST,
+                         win32con.WM_SYSCOMMAND, win32con.SC_MONITORPOWER, -1)
 
 
 def main():
@@ -34,7 +46,6 @@ def main():
 
     if parsec_running:
         ser.write(b"RUNNING")
-        print("Parsec running")
 
     else:
         ser.write(b"ERROR")
--- Sketch/Ino.h
+++ Sketch/Ino.h
@@ -7,6 +7,15 @@
 namespace INO
 {
 
+#define SWITCH 12
+
+  void powerOn()
+  {
+    digitalWrite(SWITCH, LOW);
+    delay(1000);
+    digitalWrite(SWITCH, HIGH);
+  }
+
   void triggerAutomation()
   {
 
@@ -28,26 +37,30 @@ namespace INO
 
   void setup()
   {
+
+    pinMode(SWITCH, OUTPUT);
+
     Node.begin(115200);
     Console.begin(9600);
     Keyboard.begin();
 
-    while (!Console)
-    {
-      delay(1);
-    }
+    delay(1000);
 
     Console.println("Ready");
   }
 
   void loop()
   {
+    digitalWrite(SWITCH, HIGH);
+
     if (Node.available() > 0)
     {
       CMD command = readNode();
 
       if (command == CMD::START)
       {
+        powerOn();
+        delay(30000);
         triggerAutomation();
       }
       Node.flush();
